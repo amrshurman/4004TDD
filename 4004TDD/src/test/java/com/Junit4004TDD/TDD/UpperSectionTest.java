b33022c69aaa1907e44c81bd5a822c5efd2ff953
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-07 18:58:14 -0400

    added failed test

--- 4004TDD/src/test/java/com/Junit4004TDD/TDD/UpperSectionTest.java
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/UpperSectionTest.java
@@ -18,4 +18,19 @@ public class UpperSectionTest extends TestCase{
 		System.out.println(NewDiceGroup.get(1).getNumber());
 		assertEquals(NewDiceGroup.get(0).getNumber(),2);
 	}
+	
+	public void testCheckUpperAddition2() { //should take two 2's and three 3's
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		Dice d3 = new Dice();
+		Dice d4 = new Dice();
+		Dice d5 = new Dice();
+		d1.setNumber(2); d2.setNumber(2); d3.setNumber(3); d4.setNumber(3); d5.setNumber(3);
+		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+		DiceGroup.add(d1);DiceGroup.add(d2);DiceGroup.add(d3);DiceGroup.add(d4);DiceGroup.add(d5);
+		DiceGroup dc= new DiceGroup();
+		ArrayList<Dice> NewDiceGroup= dc.upperAddition(DiceGroup);
+		System.out.println(NewDiceGroup.size());
+		assertEquals(NewDiceGroup.size(),5);
+	}
 }
