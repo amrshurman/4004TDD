2b83499bfd6c5947691c715b3482fd8402713ff1
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-07 17:52:56 -0400

    test for rolling all 5 dice

--- 4004TDD/src/test/java/com/Junit4004TDD/TDD/DiceTest.java
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/DiceTest.java
@@ -4,13 +4,19 @@ import junit.framework.TestCase;
 import static org.junit.Assert.assertNotEquals;
 public class DiceTest extends TestCase{
 	
-	public void testRoll() {
+	public void testRoll() { //tests whether a roll is possible or not
 	Dice d = new Dice();
 	int i=0;
 	i = d.roll();
-	assertNotEquals(i,0);
+	assertNotEquals(i,0); //if roll works then we should get a NON ZERO value as a roll
 	}
 	
-	
+	public void testGroupRoll() {
+		DiceGroup dg = new DiceGroup();
+		int i = 0;
+		dg.groupRoll();
+		i=dg.getTotal();
+		assertNotEquals(i,0);
+	}
 	
 }
