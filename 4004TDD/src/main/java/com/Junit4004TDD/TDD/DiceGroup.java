e405ec932747d440c9ba0c5e4dc19f1b93a4b465
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-07 18:54:05 -0400

    code refactor. Make previous method return list of dice rather than total number

--- 4004TDD/src/main/java/com/Junit4004TDD/TDD/DiceGroup.java
+++ 4004TDD/src/main/java/com/Junit4004TDD/TDD/DiceGroup.java
@@ -1,48 +1,50 @@
 package com.Junit4004TDD.TDD;
+
 import java.util.ArrayList;
 
-public class DiceGroup { //class for GROUP of dice
-ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+public class DiceGroup { // class for GROUP of dice
+	ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
 
-public DiceGroup() { //initializes the total 5 dice 
-	for (int i=0;i<5;i++) {
-		Dice d = new Dice();
-		DiceGroup.add(d);
+	public DiceGroup() { // initializes the total 5 dice
+		for (int i = 0; i < 5; i++) {
+			Dice d = new Dice();
+			DiceGroup.add(d);
+		}
 	}
-}
 
-public void groupRoll(ArrayList<Dice> d) { //roll all selected dice
-for (int i=0;i<d.size();i++) {
-	d.get(i).roll();
-}
-}
-
-public int getTotal(ArrayList<Dice> d) { //get total number of selected dice
-	int t=0;
-	for (int i=0;i<d.size();i++) {
-		t+=d.get(i).getNumber();
+	public void groupRoll(ArrayList<Dice> d) { // roll all selected dice
+		for (int i = 0; i < d.size(); i++) {
+			d.get(i).roll();
+		}
 	}
-	return t;
-}
 
-public int upperAddition(ArrayList<Dice> d) { //adds the upper addition part of the score sheet
-	int finalCount = 0;
-	int finalTarget = 0;
-	for (int i = 0;i<d.size();i++) {
-		int count = 0;
-		int target = 0;
-		target = d.get(i).getNumber();
-		for (int j = 0;j<d.size();j++) {
-			if (target== d.get(j).getNumber()) {
-				count++;
+	public int getTotal(ArrayList<Dice> d) { // get total number of selected dice
+		int t = 0;
+		for (int i = 0; i < d.size(); i++) {
+			t += d.get(i).getNumber();
+		}
+		return t;
+	}
+
+	public ArrayList<Dice> upperAddition(ArrayList<Dice> d) { // adds the upper addition part of the score sheet
+		ArrayList<Dice> fd = new ArrayList<Dice>();
+
+		for (int i = 0; i < d.size(); i++) {
+			int count = 0;
+			int target = 0;
+			target = d.get(i).getNumber();
+			for (int j = 0; j < d.size(); j++) {
+				if (target == d.get(j).getNumber()) {
+					count++;
+				}
+			}
+			if (count > 1) {
+				for (int k = 0; k < count; k++) {
+					fd.add(d.get(i));
+				}
 			}
 		}
-		if (count>finalCount) { 
-			finalCount=count;
-			finalTarget=target;
-		}
+		return fd;
 	}
-	return (finalTarget * finalCount);
-}
 
 }
--- 4004TDD/src/test/java/com/Junit4004TDD/TDD/UpperSectionTest.java
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/UpperSectionTest.java
@@ -6,7 +6,7 @@ import junit.framework.TestCase;
 
 public class UpperSectionTest extends TestCase{
 
-	public void testCheckUpperAddition() {
+	public void testCheckUpperAddition() { //should take two 2's and disregard the 3
 		Dice d1 = new Dice();
 		Dice d2 = new Dice();
 		Dice d3 = new Dice();
@@ -14,8 +14,8 @@ public class UpperSectionTest extends TestCase{
 		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
 		DiceGroup.add(d1);DiceGroup.add(d2);DiceGroup.add(d3);
 		DiceGroup dc= new DiceGroup();
-		int i= dc.upperAddition(DiceGroup);
-		System.out.println(i);
-		assertEquals(i,4);
+		ArrayList<Dice> NewDiceGroup= dc.upperAddition(DiceGroup);
+		System.out.println(NewDiceGroup.get(1).getNumber());
+		assertEquals(NewDiceGroup.get(0).getNumber(),2);
 	}
 }
