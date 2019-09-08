aafd3efd4377f20cfeb4f13c084433023071a3ab
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-07 22:33:35 -0400

    added 2 tests for sequences which both passed

--- 4004TDD/src/test/java/com/Junit4004TDD/TDD/LowerSectionTest.java
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/LowerSectionTest.java
@@ -59,4 +59,32 @@ public class LowerSectionTest extends TestCase{
 		ArrayList<Dice> NewDiceGroup= dc.sequence(DiceGroup);
 		assertEquals(NewDiceGroup.size(),4); //size of group should be 4
 	}
+	public void testSmallSeq2() { //should take 1,2,3,4
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		Dice d3 = new Dice();
+		Dice d4 = new Dice();
+		Dice d5 = new Dice();
+		
+		d1.setNumber(1); d2.setNumber(3); d3.setNumber(4);d4.setNumber(5);d5.setNumber(6);
+		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+		DiceGroup.add(d1);DiceGroup.add(d2); DiceGroup.add(d3); DiceGroup.add(d4); DiceGroup.add(d5); 
+		DiceGroup dc= new DiceGroup();
+		ArrayList<Dice> NewDiceGroup= dc.sequence(DiceGroup);
+		assertEquals(NewDiceGroup.size(),4); //size of group should be 4
+	}
+	public void testSmallSeq3() { //should take 1,2,3,4,5
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		Dice d3 = new Dice();
+		Dice d4 = new Dice();
+		Dice d5 = new Dice();
+		
+		d1.setNumber(4); d2.setNumber(6); d3.setNumber(2);d4.setNumber(5);d5.setNumber(3);
+		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+		DiceGroup.add(d1);DiceGroup.add(d2); DiceGroup.add(d3); DiceGroup.add(d4); DiceGroup.add(d5); 
+		DiceGroup dc= new DiceGroup();
+		ArrayList<Dice> NewDiceGroup= dc.sequence(DiceGroup);
+		assertEquals(NewDiceGroup.size(),5); //size of group should be 5
+	}
 }
