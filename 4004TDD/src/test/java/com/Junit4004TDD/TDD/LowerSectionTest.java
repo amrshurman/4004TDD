6923400d93d877b74e412ea128973d3e43caaab8
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-07 21:47:36 -0400

    added test for yahtzee and passed

--- 4004TDD/src/test/java/com/Junit4004TDD/TDD/LowerSectionTest.java
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/LowerSectionTest.java
@@ -15,7 +15,6 @@ public class LowerSectionTest extends TestCase{
 		DiceGroup.add(d1);DiceGroup.add(d2); DiceGroup.add(d3); 
 		DiceGroup dc= new DiceGroup();
 		ArrayList<Dice> NewDiceGroup= dc.OfKind(DiceGroup);
-		System.out.println("Size: " + NewDiceGroup.size());
 		assertEquals(NewDiceGroup.size(),3); //size of group should be 3
 	}
 	public void test4ofKind() { //should take all twos as four of a kind
@@ -29,7 +28,20 @@ public class LowerSectionTest extends TestCase{
 		DiceGroup.add(d1);DiceGroup.add(d2); DiceGroup.add(d3); DiceGroup.add(d4); 
 		DiceGroup dc= new DiceGroup();
 		ArrayList<Dice> NewDiceGroup= dc.OfKind(DiceGroup);
-		System.out.println("Size: " + NewDiceGroup.size());
 		assertEquals(NewDiceGroup.size(),4); //size of group should be 4
+	}
+	public void testYahtzee() { //should take all 5
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		Dice d3 = new Dice();
+		Dice d4 = new Dice();
+		Dice d5 = new Dice();
+		
+		d1.setNumber(2); d2.setNumber(2); d3.setNumber(2);d4.setNumber(2);d5.setNumber(2);
+		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+		DiceGroup.add(d1);DiceGroup.add(d2); DiceGroup.add(d3); DiceGroup.add(d4); DiceGroup.add(d5); 
+		DiceGroup dc= new DiceGroup();
+		ArrayList<Dice> NewDiceGroup= dc.OfKind(DiceGroup);
+		assertEquals(NewDiceGroup.size(),5); //size of group should be 5
 	}
 }
