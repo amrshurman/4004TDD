00c8fb12efca3723ffb998240276f047409fac50
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-07 21:50:21 -0400

    added test for sequence

--- 4004TDD/src/test/java/com/Junit4004TDD/TDD/LowerSectionTest.java
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/LowerSectionTest.java
@@ -44,4 +44,19 @@ public class LowerSectionTest extends TestCase{
 		ArrayList<Dice> NewDiceGroup= dc.OfKind(DiceGroup);
 		assertEquals(NewDiceGroup.size(),5); //size of group should be 5
 	}
+	
+	public void testSmallSeq() { //should take 3,4,5,6
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		Dice d3 = new Dice();
+		Dice d4 = new Dice();
+		Dice d5 = new Dice();
+		
+		d1.setNumber(1); d2.setNumber(3); d3.setNumber(4);d4.setNumber(5);d5.setNumber(6);
+		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+		DiceGroup.add(d1);DiceGroup.add(d2); DiceGroup.add(d3); DiceGroup.add(d4); DiceGroup.add(d5); 
+		DiceGroup dc= new DiceGroup();
+		ArrayList<Dice> NewDiceGroup= dc.sequence(DiceGroup);
+		assertEquals(NewDiceGroup.size(),4); //size of group should be 4
+	}
 }
