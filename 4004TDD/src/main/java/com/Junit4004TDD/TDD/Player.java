88b4ec8dc9d427b5280b59fe42ce01aa7397242a
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-10 19:17:05 -0400

    added functionality + test passed

--- 4004TDD/src/main/java/com/Junit4004TDD/TDD/Player.java
+++ 4004TDD/src/main/java/com/Junit4004TDD/TDD/Player.java
@@ -21,7 +21,18 @@ public class Player {
 	public int TotalLS = 0;
 	public int TotalUS = 0;
 	public int GrandTotal = 0;
-
+	public boolean acesB=false;
+	public boolean twosB=false;
+	public boolean threesB=false;
+	public boolean foursB=false;
+	public boolean fivesB=false;
+	public boolean sixesB=false;
+	public boolean ThreeKB=false;
+	public boolean FourKB=false;
+	public boolean smB=false;
+	public boolean lB=false;
+	public boolean yahtzeeB=false;
+	
 	public Player(String name) {
 		this.name = name;
 	}
@@ -53,5 +64,51 @@ public class Player {
 	public int GrandTotal() {
 		GrandTotal = TotalUS + TotalLS;
 		return GrandTotal;
+	}
+	public void set(int i,int j) {
+		if ((i==1)&&(acesB==false)) {
+			aces=j;
+			acesB=true;
+		}
+		if ((i==2)&&(twosB==false)) {
+			twos=j;
+			twosB=true;
+		}
+		if ((i==3)&&(threesB==false)) {
+			threes=j;
+			threesB=true;
+		}
+		if ((i==4)&&(foursB==false)) {
+			fours=j;
+			foursB=true;
+		}
+		if ((i==5)&&(fivesB==false)) {
+			fives=j;
+			fivesB=true;
+		}
+		if ((i==6)&&(sixesB==false)) {
+			sixes=j;
+			sixesB=true;
+		}
+		if ((i==7)&&(ThreeKB==false)) {
+			threeOfKind=j;
+			ThreeKB=true;
+		}
+		if ((i==8)&&(FourKB==false)) {
+			fourOfKind=j;
+			FourKB=true;
+		}
+		if ((i==9)&&(smB==false)) {
+			smStraight=j;
+			smB=true;
+		}
+		if ((i==10)&&(lB==false)) {
+			lgStraight=j;
+			lB=true;
+		}
+		if ((i==11)&&(yahtzeeB==false)) {
+			Yahtzee=j;
+			yahtzeeB=true;
+		}
 	}
 }
