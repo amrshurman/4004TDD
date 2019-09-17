a30d5ce44aaf0b866c7f9890f434a44afc1d9a31
amrshurman <amrshurman@cmail.carleton.ca>
2019-09-17 14:23:12 -0400

    added test

---
+++ 4004TDD/src/test/java/com/Junit4004TDD/TDD/YahtzeeBonusTest.java
@@ -0,0 +1,27 @@
+package com.Junit4004TDD.TDD;
+
+import junit.framework.TestCase;
+import java.util.ArrayList;
+
+public class YahtzeeBonusTest extends TestCase{
+	public void testYahtzeeBonusScore2() { //test for long yahtzee bonus score if spot in upper section is taken
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		Dice d3 = new Dice();
+		Dice d4 = new Dice();
+		Dice d5 = new Dice();
+		Player p1=new Player("p1");
+		d1.setNumber(2); d2.setNumber(2); d3.setNumber(2); d4.setNumber(2);d5.setNumber(2);
+		ArrayList<Dice> DiceGroup = new ArrayList<Dice>();
+		DiceGroup.add(d1);DiceGroup.add(d2);DiceGroup.add(d3);DiceGroup.add(d4);DiceGroup.add(d5);
+		p1.dg.DiceGroup=DiceGroup;
+		p1.dg.suggestions(p1);
+		p1.dg.scoreSetter(p1,"y");
+		p1.dg.suggestions(p1); //score another yahtzee with same dice
+		p1.dg.scoreSetter(p1,"y");
+		p1.dg.suggestions(p1); //score another yahtzee with same dice
+		p1.dg.scoreSetter(p1,"y");
+		p1.dg.printDice();
+		assertEquals(p1.YahtzeeBonus,200);
+	}
+}
